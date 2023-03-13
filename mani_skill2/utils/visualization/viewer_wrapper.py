b0bbd4cfc2fa0090c5f37e6a689fc0b8ebdc43d1
Kolin Guo <ruguo@ucsd.edu>
2023-03-13 16:52:38 -0700

    Add viewer_wrapper for rendering points

--- mani_skill2/utils/visualization/viewer_wrapper.py
+++ mani_skill2/utils/visualization/viewer_wrapper.py
@@ -9,7 +9,7 @@ from mani_skill2.utils.sapien_utils import get_pairwise_contacts
 class SapienViewerWrapper(gym.Wrapper):
     def __init__(self, env: gym.Env):
         super().__init__(env)
-        self.render_vectors = OrderedDict()  # {name: R.Node}
+        self.render_nodes = OrderedDict()  # {name: R.Node}
         # [(actor0, actor1, separate_contact, scale, color)]
         self.render_contacts = []
 
@@ -30,6 +30,9 @@ class SapienViewerWrapper(gym.Wrapper):
 
         self.cone = self.viewer.cone
         self.capsule = self.viewer.capsule
+        self.sphere = renderer_context.create_capsule_mesh(0.1, 0, 16, 4)
+
+        colors = ["red", "green", "blue", "yellow", "cyan", "magenta"]
 
         self.materials = {
             'red': self.viewer.mat_red,
@@ -66,19 +69,32 @@ class SapienViewerWrapper(gym.Wrapper):
             'cyan': self.viewer.cyan_capsule,
             'magenta': self.viewer.magenta_capsule,
         }
+        self.spheres = {
+            color: renderer_context.create_model(
+                [self.sphere], [self.materials[color]]
+            ) for color in colors
+        }
 
         self.models_created = True
 
     @staticmethod
-    def _set_pos_heading_scale(node: R.Node, pos, heading, scale=[0.1,0.1,0.1],
-                               base_heading=[1.,0.,0.]):
-        base_heading, heading = np.array(base_heading), np.array(heading)
-
-        node.set_position(pos)
-        node.set_rotation(
-            rotation_between_vec(base_heading, heading).as_quat()[[3, 0, 1, 2]]
-        )
-        node.set_scale(scale)
+    def _set_pose_scale(node: R.Node, pos=None, heading=None, scale=None,
+                        pose=None, base_heading=[1.,0.,0.]):
+        if pos is not None:
+            assert pose is None, 'pose must be None'
+            node.set_position(pos)
+        if heading is not None:
+            assert pose is None, 'pose must be None'
+            base_heading, heading = np.array(base_heading), np.array(heading)
+            node.set_rotation(
+                rotation_between_vec(base_heading, heading).as_quat()[[3, 0, 1, 2]]
+            )
+        if pose is not None:
+            assert pos is None and heading is None, 'pos and heading must be None'
+            node.set_position(pose.p)
+            node.set_rotation(pose.q)
+        if scale is not None:
+            node.set_scale(scale)
 
     def _add_vector(self, pos, heading, scale=[0.1,0.1,0.1],
                     color='red', name=None):
@@ -86,7 +102,7 @@ class SapienViewerWrapper(gym.Wrapper):
             self._create_visual_models()
 
         if name is None:
-            name = f"vector_{len(self.render_vectors)}"
+            name = f"vector_{len(self.render_nodes)}"
 
         render_scene: R.Scene = self.viewer.scene.renderer_scene._internal_scene
         node = render_scene.add_node()
@@ -105,23 +121,63 @@ class SapienViewerWrapper(gym.Wrapper):
         obj.cast_shadow = False
         obj.transparency = 0
 
-        self._set_pos_heading_scale(node, pos, heading, scale)
-        self.render_vectors[name] = node
+        self._set_pose_scale(node, pos, heading, scale)
+        self.render_nodes[name] = node
 
-    def toggle_vector(self, name, show: bool):
-        for c in self.render_vectors[name].children:
+    def toggle_render_node(self, name, show: bool):
+        for c in self.render_nodes[name].children:
             c.transparency = 1 - int(show)
 
     def update_vectors(self, vectors: dict[str, dict]):
         """If not exist, call _add_vector. Else, update vector properties"""
         for name, vector_kwargs in vectors.items():
-            if name not in self.render_vectors:
+            if name not in self.render_nodes:
                 self._add_vector(name=name, **vector_kwargs)
             else:
                 vector_kwargs.pop("color", None)  # cannot update color
-                self._set_pos_heading_scale(self.render_vectors[name],
-                                            **vector_kwargs)
-                self.toggle_vector(name, show=True)
+                self._set_pose_scale(self.render_nodes[name], **vector_kwargs)
+                self.toggle_render_node(name, show=True)
+
+    def _add_point(self, pos, scale=[1,1,1], color='red', name=None):
+        if not self.models_created:
+            self._create_visual_models()
+
+        if name is None:
+            name = f"pt_{len(self.render_nodes)}"
+
+        render_scene: R.Scene = self.viewer.scene.renderer_scene._internal_scene
+        node = render_scene.add_node()
+
+        obj = render_scene.add_object(self.spheres[color], node)
+        obj.set_scale(scale)
+        obj.shading_mode = 2
+        obj.cast_shadow = False
+        obj.transparency = 0
+
+        self._set_pose_scale(node, pos)
+        self.render_nodes[name] = node
+
+    def update_points(self, points: dict[str, dict]={}, scale=np.ones(3)):
+        """If not exist, call _add_point. Else, update point properties"""
+        scale = self.viewer.axes_scale * scale
+        if len(points) == 0:  # update scale based on axes_scale
+            for name, node in self.render_nodes.items():
+                if name.startswith("pt_"):
+                    self._set_pose_scale(node, scale=scale)
+
+        for name, points_kwargs in points.items():
+            color = points_kwargs.pop("color", "red")
+            points_pos = points_kwargs["pos"].reshape(-1, 3)
+
+            for i, pos in enumerate(points_pos):  # for each point
+                pt_name = f"pt_{name}_{i}"
+
+                if pt_name not in self.render_nodes:
+                    self._add_point(pos, scale=scale, color=color, name=pt_name)
+                else:
+                    self._set_pose_scale(self.render_nodes[pt_name],
+                                         pos, scale=scale)
+                    self.toggle_render_node(pt_name, show=True)
 
     def show_contact_visualization(self, actor0: ActorBase, actor1: ActorBase,
                                    draw_separate=True,
@@ -173,9 +229,9 @@ class SapienViewerWrapper(gym.Wrapper):
                     contact_impulse_norms.append(np.linalg.norm(impulse))
 
             # Hide previous contact impulse vectors
-            for name in self.render_vectors:
+            for name in self.render_nodes:
                 if name.startswith(f"{actor0.name}_{actor1.name}_"):
-                    self.toggle_vector(name, show=False)
+                    self.toggle_render_node(name, show=False)
 
             # Draw impulse vectors
             vectors = {
@@ -193,5 +249,6 @@ class SapienViewerWrapper(gym.Wrapper):
     def render(self, mode='human', **kwargs):
         if mode == 'human':
             self.update_contact_vectors()
+            self.update_points()
 
         return self.env.render(mode=mode, **kwargs)
